7354fdffcc039e46709797ca10072783d9e96b3d
Michal Helgert <michal.helgert@seznam.cz>
2022-05-02 15:36:18 +0200

    Přidejte soubory projektu.

---
+++ MetroAplikace/LinkaMetra.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroAplikace
+{
+    internal class LinkaMetra
+    {
+        private List<string> _nazvyStanic;
+
+        public LinkaMetra(List<string> _vkladaneStanice)
+        {
+            _nazvyStanic = _vkladaneStanice; //do listu na 11. řádku načteme list, předaný v parametru 
+        }
+        public void VypisStanice()
+        {
+            foreach(string stanice in _nazvyStanic) //projede všechny stanice v Listu _nazvyStanic
+            {
+                Console.WriteLine(stanice); //vypisuje současnou stanici v cyklu
+            }
+        }
+        public void PrvniPosledniStanice()
+        {
+            string prvniStanice = _nazvyStanic[0];
+            string posledniStanice = _nazvyStanic[_nazvyStanic.Count - 1];
+            Console.WriteLine("----------------");
+            Console.WriteLine($"První stanice: {prvniStanice}  -  Poslední stanice: {posledniStanice}");
+            //dolar = alt gr + ů
+
+        }
+        public int ZjistiCas(string odjezdStanice, string prijezdStanice)
+        {
+            int cas = 0;
+            if(_nazvyStanic.Contains(prijezdStanice) && _nazvyStanic.Contains(odjezdStanice))
+            {
+                int indexOdjezd = _nazvyStanic.IndexOf(odjezdStanice);
+                int indexPrijezd = _nazvyStanic.IndexOf(prijezdStanice);
+                int rozdil = indexOdjezd - indexPrijezd;
+                cas = rozdil * 2;
+            }
+            else
+            {
+                Console.WriteLine("Chyba - stanice nenalezeny");
+            }
+
+            return Math.Abs(cas); //absolutní hodnota (z -5 udělá číslo 5. Z čísla 5 udělá zase 5)
+        }
+
+    }
+}
---
+++ MetroAplikace/Program.cs
@@ -0,0 +1,59 @@
+﻿using MetroAplikace;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@" |  \/  |    | |            
+ | \  / | ___| |_ _ __ ___  
+ | |\/| |/ _ \ __| '__/ _ \ 
+ | |  | |  __/ |_| | | (_) |
+ |_|  |_|\___|\__|_|  \___/ 
+                           ");
+Console.ResetColor();
+
+List<string> linkaA = new List<string>
+{
+    "Nemocnice Motol", "Petřiny", "Nádraží Veleslavín", "Bořislavka", "Dejvická", "Hradčanská",
+    "Malostranská", "Staroměstská", "Muzeum", "Můstek", "Náměstí Míru", "Jiřího z Poděbrad",
+    "Flora", "Želivského", "STRAŠNICKÁ", "Skalka", "Depo Hostivař"
+};
+List<string> linkaD = new List<string>
+{
+    "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč",
+    "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru"
+};
+
+LinkaMetra linkaMetroA = new LinkaMetra(linkaA);
+LinkaMetra linkaMetroD = new LinkaMetra(linkaD);
+
+Console.WriteLine("Vyberte linku metra  A/D");
+string vybranaLinka = Console.ReadLine();
+if (vybranaLinka == "A" || vybranaLinka == "D")
+{
+    if (vybranaLinka == "A")
+    {
+        linkaMetroA.VypisStanice();
+        linkaMetroA.PrvniPosledniStanice();
+    }
+    if (vybranaLinka == "D")
+    {
+        linkaMetroD.VypisStanice();
+        linkaMetroD.PrvniPosledniStanice();
+    }
+}
+
+Console.WriteLine("Zadej stanici odjezdu");
+string staniceA = Console.ReadLine();
+Console.WriteLine("Zadej stanici příjezdu");
+string staniceB = Console.ReadLine();
+
+int casCesty = 0;
+if (vybranaLinka == "A")
+{
+    casCesty = linkaMetroA.ZjistiCas(staniceA, staniceB);
+}
+else if(vybranaLinka=="D")
+{
+    casCesty = linkaMetroD.ZjistiCas(staniceA, staniceB);
+}
+Console.WriteLine($"Čas cesty je přibližně: {casCesty} minut");
+
+
